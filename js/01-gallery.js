1e7fede0d8d3d433e4be44f84f66b8937ae2dc74
ElenaFarina <121393458+ElenaFarina@users.noreply.github.com>
2023-05-31 12:41:33 +0300

    start1

---
+++ js/01-gallery.js
@@ -0,0 +1,46 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const galleryItemsE1 = galleryItems
+    .map(
+        ({ preview, original, description }) =>
+        `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+    </a>
+    </li>`
+    )
+    .join('');
+        const galleryE1 = document.querySelector(".gallery");
+        galleryE1.insertAdjacentHTML("beforeend", galleryItemsE1);
+
+galleryE1.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const target = e.target;
+    if (!target.classList.contains("gallery__image")) return;
+
+    const imageScr = target.dataset.source;
+    const instance = basicLightbox.create(
+        `<img src="${imageScr}" width="800" height="600">`,
+    {
+        onClose: () => {
+        document.removeEventListener("keydown", handleKeyDown);
+    },
+    onShow: () => {
+        document.addEventListener("keydown", handleKeyDown);
+    }
+    }
+            );
+            instance.show();
+        })
+    function handleKeyDown(e) {
+    if (e.code === "Escape") {
+    instance.close();
+    }
+}
+console.log(galleryItems);
